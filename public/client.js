3ab5e90471f58dd77a93616bd856bc69573fd06b
Glitch (semantic-cursor) <none>
2019-12-10 15:28:57 +0000

    hot cold hints

--- public/client.js
+++ public/client.js
@@ -1,5 +1,3 @@
-const modelURL =
-  "https://cdn.glitch.com/670bd0c3-4761-4b40-8adc-a36dee36c3cb%2Fwordvecs5000.json?v=1575425512135";
 const modelURLsm =
   "https://cdn.glitch.com/670bd0c3-4761-4b40-8adc-a36dee36c3cb%2Fwordvecs1000.json?v=1575419642687";
 
@@ -12,9 +10,9 @@ const targetContext = document.getElementById("targetContext");
 
 var state = {};
 var nearValues = [];
+var prevDistance;
 
 var socket = io();
-
 socket.on("state", data => checkState(data));
 var word2Vec = ml5.word2vec(modelURLsm, onLoad);
 
@@ -25,6 +23,7 @@ function onLoad() {
 }
 
 function checkState(data) {
+  // if missing cursor or target, send new one to server
   if (!data.cursor) {
     word2Vec.getRandomWord().then(word => socket.emit("newCursor", word));
   }
@@ -33,12 +32,46 @@ function checkState(data) {
     word2Vec.getRandomWord().then(word => socket.emit("newTarget", word));
   }
 
-  history.innerHTML = data.history.join(" ");
+  // update state object
 
-  state = data;
+  state.target = data.target;
+  state.history = data.history;
+  let targetTensor = word2Vec.model[state.target];
 
+  // set first prevDistance from target
+  if (!state.cursor && data.cursor && state.target) {
+    let newCursorTensor = word2Vec.model[data.cursor];
+
+    prevDistance = newCursorTensor
+      .sub(targetTensor)
+      .norm()
+      .dataSync();
+  }
+
+  // calculate if moving closer or further
+  if (state.cursor && data.cursor) {
+    let oldCursor = word2Vec.model[state.cursor];
+    let newCursor = word2Vec.model[data.cursor];
+
+    let newMag = newCursor
+      .sub(targetTensor)
+      .norm()
+      .dataSync();
+
+    if (newMag > prevDistance) {
+      flashCold();
+    } else if (newMag < prevDistance) {
+      flashHot();
+    }
+    prevDistance = newMag;
+  }
+
+  state.cursor = data.cursor;
+
+  // render
   renderCursor(state.cursor);
   renderTarget(state.target);
+  history.innerHTML = state.history.join(" ");
 }
 
 function renderCursor(data) {
@@ -46,10 +79,11 @@ function renderCursor(data) {
 
   word2Vec.nearest(data).then(results => {
     nearValues = results.map(obj => obj.word);
-    console.log(nearValues);
+
     [...document.getElementsByClassName("nearbyWord")].forEach(
       (element, index) => {
         element.innerHTML = nearValues[index];
+
         if (state.history.includes(nearValues[index])) {
           element.style.color = "#551A8B";
         } else {
@@ -75,18 +109,26 @@ textForm.onsubmit = event => {
   event.preventDefault();
 
   if (nearValues.includes(textInput.value)) {
-    
     socket.emit("newCursor", textInput.value);
-    
+
     if (state.target === textInput.value) {
-      document.body.classList = ['blink'];
-      setTimeout(() => document.body.classList = [], 2000);
+      flashFun();
     }
-    
+
     textInput.value = "";
     textInput.focus();
   }
-  
-  
-  
 };
+
+function flashFun() {
+  document.body.classList = ["blink"];
+  setTimeout(() => (document.body.classList = []), 2000);
+}
+function flashHot() {
+  document.body.classList = ["hot"];
+  setTimeout(() => (document.body.classList = []), 2000);
+}
+function flashCold() {
+  document.body.classList = ["cold"];
+  setTimeout(() => (document.body.classList = []), 2000);
+}
